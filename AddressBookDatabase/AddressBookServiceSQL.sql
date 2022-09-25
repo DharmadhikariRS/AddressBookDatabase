9943fab4b19b8477fe672bd4080fcf8f3195f810
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-09-25 22:09:43 +0530

    filter data by city and state

--- AddressBookDatabase/AddressBookServiceSQL.sql
+++ AddressBookDatabase/AddressBookServiceSQL.sql
@@ -15,10 +15,14 @@ Email varchar(20),
 select * from AddressBook
 ------UC3--------
 insert into AddressBook values('Prithvi','Pandey','House 124','Mumbai','Maharashtra',123456,99992222333,'pp@p.in'),
-('Kriti','singh','House 101','NewDelhi','Delhi',551155,88994422333,'sp@p.in')
+('Kriti','singh','House 101','NewDelhi','Delhi',551155,88994422333,'sp@p.in'),
 ------UC4--------
 update AddressBook set PhoneNumber=8888555566 where FirstName='Kriti'
 ------UC5--------
 delete from AddressBook where FirstName='Prithvi'
 ------UC6--------
 select * from AddressBook where City='NewDelhi' or state= 'Maharashtra'
+------UC7--------
+insert into AddressBook values ('Sidharth','Patra','House 111','Pune','Maharashtra',415415,7799557722,'sp@pp.in'),('Rima','Barde','House 54','Thane','Maharashtra',000001,8822336655,'rb@rb.com');
+
+select count (*) from AddressBook where City='Pune' and State='Maharashtra'
